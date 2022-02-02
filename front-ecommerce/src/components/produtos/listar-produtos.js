ada00b2a59f455846a6e7cba3f73ae74c5cd6a0c
Daniella Dutra <danivetdutra@gmail.com>
2022-02-02 16:05:50 -0300

    Criado componente de listagem de produtos

---
+++ front-ecommerce/src/components/produtos/listar-produtos.js
@@ -0,0 +1,22 @@
+import placeholder from '../../images/286x180.png';
+import { Card } from 'react-bootstrap';
+import Button from 'react-bootstrap';
+
+function ListarProdutos() {
+   const produtos = [
+    { nome: 'Aprenda Java', preco: 'R$ 59,99' },
+		{ nome: 'JavaScript em 24 horas', preco: 'R$ 19,99' },
+		{ nome: 'React em 7 dias', preco: 'R$ 29,99' },
+		{ nome: 'Algoritmos e Estrutura de Dados', preco: 'R$ 25,99' },
+		{ nome: 'Start-Up', preco: 'R$ 29,99' },
+		{ nome: 'Testes Unitários com Jasmine', preco: 'R$ 14,99' },
+		{ nome: 'APIs RESTful com Spring e Java', preco: 'R$ 15,99' },
+		{ nome: 'TypeScript na prática', preco: 'R$ 9,99' }
+  ];
+
+  return();
+
+}
+
+
+export default ListarProdutos;
