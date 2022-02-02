a4471371fe0f3fc78faef326bef8fca78b02348f
Daniella Dutra <danivetdutra@gmail.com>
2022-02-02 16:22:03 -0300

    Criado html do card de produtos

--- front-ecommerce/src/components/produtos/listar-produtos.js
+++ front-ecommerce/src/components/produtos/listar-produtos.js
@@ -1,22 +1,36 @@
 import placeholder from '../../images/286x180.png';
-import { Card } from 'react-bootstrap';
+import { Card, CardImg } from 'react-bootstrap';
 import Button from 'react-bootstrap';
+import { func } from 'prop-types';
 
 function ListarProdutos() {
-   const produtos = [
+  const produtos = [
     { nome: 'Aprenda Java', preco: 'R$ 59,99' },
-		{ nome: 'JavaScript em 24 horas', preco: 'R$ 19,99' },
-		{ nome: 'React em 7 dias', preco: 'R$ 29,99' },
-		{ nome: 'Algoritmos e Estrutura de Dados', preco: 'R$ 25,99' },
-		{ nome: 'Start-Up', preco: 'R$ 29,99' },
-		{ nome: 'Testes Unitários com Jasmine', preco: 'R$ 14,99' },
-		{ nome: 'APIs RESTful com Spring e Java', preco: 'R$ 15,99' },
-		{ nome: 'TypeScript na prática', preco: 'R$ 9,99' }
+    { nome: 'JavaScript em 24 horas', preco: 'R$ 19,99' },
+    { nome: 'React em 7 dias', preco: 'R$ 29,99' },
+    { nome: 'Algoritmos e Estrutura de Dados', preco: 'R$ 25,99' },
+    { nome: 'Start-Up', preco: 'R$ 29,99' },
+    { nome: 'Testes Unitários com Jasmine', preco: 'R$ 14,99' },
+    { nome: 'APIs RESTful com Spring e Java', preco: 'R$ 15,99' },
+    { nome: 'TypeScript na prática', preco: 'R$ 9,99' },
   ];
 
-  return();
+  function render() {
+    <Card style={{ width: '18rem', margin: '10px', float: 'lefts' }}>
+      <Card.Img variant="top" src={placeholder} />
+      <Card.Body className="text-center">
+        <Card.Title style={{ height: '40px' }}>
+          {/*nome do produto*/}
+        </Card.Title>
+        <Card.Text>Descrição do produto...</Card.Text>
+        <Button variant="success" style={{ width: '100%' }}
+        onClick={}
+        >Comprar ({/*preço do produto*/})</Button>
+      </Card.Body>
+    </Card>;
+  }
 
+  return render();
 }
-
 
 export default ListarProdutos;
