50b6427a0dcab9e5af3c9d05163f13720bb03f1d
Daniella Dutra <danivetdutra@gmail.com>
2022-02-02 15:21:34 -0300

    Criado componentes do projeto

---
+++ front-ecommerce/src/components/checkout/checkout.js
@@ -0,0 +1,5 @@
+function Checkout() {
+  return <h1>Checkout</h1>;
+}
+
+export default Checkout;
--- front-ecommerce/src/mini-ecommerce-front.js
+++ front-ecommerce/src/mini-ecommerce-front.js
@@ -1,13 +1,22 @@
 import './mini-ecommerce.css';
 import { useState } from 'react';
+import Menu from './components/menu/menu';
+import Produtos from './components/produtos/produtos';
+import Checkout from './components/checkout/checkout';
 
 function MiniEcommerce() {
   const [carrinho, setCarrinho] = useState({ produtos: [] });
   const [exibirProdutos, setExibirProdutos] = useState(true);
-  const [exibirCheckout, setExibirCheckout] = useState(true);
+  const [exibirCheckout, setExibirCheckout] = useState(false);
   const [total, setTotal] = useState('0,00');
 
-  return <h1>Mini ecommerce</h1>;
+  return (
+    <div>
+      <Menu />
+      <Produtos />
+      <Checkout />
+    </div>
+  );
 }
 
 export default MiniEcommerce;
