fa28ac10f64e1af81d84cd11199f1d874559ffa1
Daniella Dutra <danivetdutra@gmail.com>
2022-02-03 10:28:19 -0300

    Implementada mensagem de sucesso ao adicionar produto ao carrinho

--- front-ecommerce/src/components/produtos/produtos.js
+++ front-ecommerce/src/components/produtos/produtos.js
@@ -1,8 +1,35 @@
 import ListarProdutos from './listar-produtos';
 import Proptypes from 'prop-types';
+import { Alert } from 'react-bootstrap';
+import { useState } from 'react';
 
 function Produtos(props) {
-  return <ListarProdutos />;
+  const [exibirMsg, setExibirMsg] = useState(false);
+  const [produto, setProduto] = useState('');
+
+  function visivel() {
+    return props.visivel ? null : 'hidden';
+  }
+
+  function exibirMensagem(produto) {
+    setExibirMsg(true);
+    setProduto(produto);
+    setTimeout(() => {
+      setExibirMsg(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={visivel}>
+      <Alert variant="success" style={{ margin: '10px' }} show={exibirMsg}>
+        <b>{produto} adicionado com sucesso ao carrinho!</b>
+      </Alert>
+      <ListarProdutos
+        exibirMensagem={exibirMensagem}
+        adicionarProduto={adicionarProduto}
+      />
+    </div>
+  );
 }
 
 Produtos.propTypes = {
