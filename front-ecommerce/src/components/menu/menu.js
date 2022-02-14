eb89726ff59c2bff47869106d95a97ad6a0f94d0
Daniella Dutra <danivetdutra@gmail.com>
2022-02-14 11:42:04 -0300

    Iniciada criaçao do menu e carrinho de compras

--- front-ecommerce/src/components/menu/menu.js
+++ front-ecommerce/src/components/menu/menu.js
@@ -1,5 +1,29 @@
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faShoppingBasket,
+  faCashRegister,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons';
+
 function Menu() {
-  return <h1>Menu</h1>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="">Mini Ecommerce</Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav>
+          <NavDropdown
+            title={
+              <div style={{ display: 'inline-block' }}>
+                <FontAwesomeIcon icon={faShoppingCart} drop="left" />
+                &nbsp; Carrinho
+              </div>
+            }
+          ></NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default Menu;
