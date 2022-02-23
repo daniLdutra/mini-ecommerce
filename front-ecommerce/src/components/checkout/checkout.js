bf44175eb229c97bff4330490bed41bfbd4e6395
Daniella Dutra <danivetdutra@gmail.com>
2022-02-23 15:13:08 -0300

    Iniciado formulario - criado campo para email

--- front-ecommerce/src/components/checkout/checkout.js
+++ front-ecommerce/src/components/checkout/checkout.js
@@ -1,13 +1,36 @@
-import { Form, Row, Col, Button, Modal, Container } from 'react-bootstrap';
+import { Form, Row, Col, Button, Modal } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import pt from 'date-fns/locale/pt';
 import PropTypes from 'prop-types';
 
-registerLocale('pt, pt');
+registerLocale('pt', pt);
 
 function Checkout(props) {
-  return <h1>Checkout</h1>;
+  return (
+    <div fluid style={{ margin: '10px' }}>
+      <h3 className="text-center">Finalizar compra</h3>
+
+      <Form noValidate style={{ margin: '10px' }}>
+        <Form.Group as={Row} controlId="email">
+          <Form.Label column sm={3}>
+            Email
+          </Form.Label>
+          <Col sm={9}>
+            <Form.Control
+              type="email"
+              placeholder="Digite o seu email"
+              name="email"
+              data-testid="txt-email"
+            />
+            <Form.Control.Feedback type="invalid">
+              Digite um email válido
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
 
 export default Checkout;
