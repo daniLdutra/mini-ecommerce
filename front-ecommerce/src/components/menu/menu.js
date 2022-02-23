9f2e9469e4c30de6b6f7381c687681c6033dd5d3
Daniella Dutra <danivetdutra@gmail.com>
2022-02-23 08:47:12 -0300

    Adicionado propTypes ao menu

--- front-ecommerce/src/components/menu/menu.js
+++ front-ecommerce/src/components/menu/menu.js
@@ -5,8 +5,9 @@ import {
   faCashRegister,
   faShoppingCart,
 } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 
-function Menu() {
+function Menu(props) {
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="">Mini Ecommerce</Navbar.Brand>
@@ -20,7 +21,7 @@ function Menu() {
               </div>
             }
           >
-            <NavDropdown.Item href="">
+            <NavDropdown.Item href="" onClick={props.handleExibirProdutos}>
               <FontAwesomeIcon icon={faShoppingBasket} />
               &nbsp;
               <strong>Produtos</strong>
@@ -31,9 +32,13 @@ function Menu() {
             <NavDropdown.Item href="" data-testid="total-carrinho">
               Total: R$ {/* chamar função de cálculo de total */}
             </NavDropdown.Item>
-            <span>
+            <span className={props.produtos.length === 0 ? 'hidden' : null}>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="" style={{ color: 'green' }}>
+              <NavDropdown.Item
+                href=""
+                style={{ color: 'green' }}
+                onClick={props.handleExibirCheckout}
+              >
                 <FontAwesomeIcon icon={faCashRegister} />
                 &nbsp; Finalizar compra
               </NavDropdown.Item>
@@ -44,5 +49,11 @@ function Menu() {
     </Navbar>
   );
 }
+
+Menu.propTypes = {
+  produtos: PropTypes.array.isRequired,
+  handleExibirProdutos: PropTypes.func.isRequired,
+  handleExibirCheckout: PropTypes.func.isRequired,
+};
 
 export default Menu;
