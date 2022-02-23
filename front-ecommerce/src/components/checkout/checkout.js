9e76898a5c518ed9749268df3a543c78d9a2adc5
Daniella Dutra <danivetdutra@gmail.com>
2022-02-23 11:48:48 -0300

    Realizada importacao de componentes,  calendario e realizada traducao para pt

--- front-ecommerce/src/components/checkout/checkout.js
+++ front-ecommerce/src/components/checkout/checkout.js
@@ -1,4 +1,12 @@
-function Checkout() {
+import { Form, Row, Col, Button, Modal, Container } from 'react-bootstrap';
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import pt from 'date-fns/locale/pt';
+import PropTypes from 'prop-types';
+
+registerLocale('pt, pt');
+
+function Checkout(props) {
   return <h1>Checkout</h1>;
 }
 
