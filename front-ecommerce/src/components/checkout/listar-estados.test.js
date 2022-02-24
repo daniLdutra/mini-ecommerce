a462b47746e55d22ebfec952a5dce9329614765e
Daniella Dutra <danivetdutra@gmail.com>
2022-02-24 11:30:01 -0300

    Criado teste para o componente de listagem de estados

---
+++ front-ecommerce/src/components/checkout/listar-estados.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import ListarEstados from './listar-estados';
+
+describe('Teste do componente de listagem de estados', () => {
+  it('deve gerar uma listagem de estados', () => {
+    const { getByTestId } = render(<ListarEstados />);
+    expect(getByTestId('AM')).toHaveTextContent('Amazonas');
+    expect(getByTestId('SP')).toHaveTextContent('São Paulo');
+  });
+});
